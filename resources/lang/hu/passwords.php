d9fa38f07a6215a5df0d7d2a173ad9a2ef9a04bb
Lily Belle <tolilybelle@gmail.com>
2015-01-25 11:39:03 +0000

    Add hu localization

---
+++ resources/lang/hu/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "A jelszónak legalább 6 karakterből kell állnia, és meg kell egyeznie a megerősítéssel.",
+	"user" => "Nem találtunk felhasználót ezzel az e-mail címmel.",
+	"token" => "Ez a jelszó-token invalid.",
+	"sent" => "Küldtünk az e-mailedre egy jelszó-helyreállító linket!",
+	"reset" => "A jelszavadat helyreállítottuk.",
+
+];
