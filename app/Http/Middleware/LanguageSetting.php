76b72cbd3b0653489c47d2c56aa2fa6f6f792823
Lily Belle <tolilybelle@gmail.com>
2015-01-25 11:49:36 +0000

    Add middleware for language setting

---
+++ app/Http/Middleware/LanguageSetting.php
@@ -0,0 +1,21 @@
+<?php namespace Rpgo\Http\Middleware;
+
+use Closure;
+
+class LanguageSetting {
+
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+        \App::setlocale('hu');
+
+        return $next($request);
+	}
+
+}
