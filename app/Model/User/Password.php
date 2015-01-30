e94da1bb0237021996ed2985b6e88a2765dbcb9a
Lily Belle <tolilybelle@gmail.com>
2015-01-30 07:00:58 +0100

    Refactor Password

--- app/Model/User/Password.php
+++ app/Model/User/Password.php
@@ -11,11 +11,21 @@ class Password {
     public function __construct($password, $hashed = false)
     {
         $this->hash = new Hash();
-        $this->password = $hashed ? $password : $this->hash->make($password);
+        $this->password = $this->password($password, $hashed);
     }
 
     public function check($password)
     {
         return $this->hash->check($password, $this->password);
+    }
+
+    /**
+     * @param string $password
+     * @param bool $hashed
+     * @return string
+     */
+    private function password($password, $hashed)
+    {
+        return $hashed ? $password : $this->hash->make($password);
     }
 }
