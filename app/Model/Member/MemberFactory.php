cc2f413c21234c6b34c479667312fe9ed6a527e9
Lily Belle <tolilybelle@gmail.com>
2015-01-30 08:00:06 +0100

    Implement one factory method for Member

--- app/Model/Member/MemberFactory.php
+++ app/Model/Member/MemberFactory.php
@@ -1,8 +1,47 @@
 <?php namespace Rpgo\Model\Member;
 
+use Rpgo\Model\Contracts\Member\MemberIdGenerator as MemberIdGeneratorContract;
 use Rpgo\Model\Contracts\Member\MemberFactory as MemberFactoryContract;
+use Rpgo\Model\Contracts\User\User;
+use Rpgo\Model\Contracts\World\World;
 
 final class MemberFactory implements MemberFactoryContract
 {
+    /**
+     * @var MemberIdGenerator
+     */
+    private $generator;
 
+    public function __construct(MemberIdGeneratorContract $generator)
+    {
+        $this->generator = $generator;
+    }
+
+    /**
+     * @param World $world
+     * @param User $user
+     * @param string $name
+     * @return Member
+     */
+    public function create(World $world, User $user, $name)
+    {
+        $id = $this->generator->next();
+        $name = new Name($name);
+        return new Member($id, $user, $world, $name);
+
+    }
+
+    /**
+     * @param string $id
+     * @param World $world
+     * @param User $user
+     * @param string $name
+     * @return Member
+     */
+    public function revive($id, World $world, User $user, $name)
+    {
+        $id = $this->generator->from($id);
+        $name = new Name($name);
+        return new Member($id, $user, $world, $name);
+    }
 }
