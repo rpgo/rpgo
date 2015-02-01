fc2334c7bccb3a26b76cfb7dae8699aa7f707897
Lily Belle <tolilybelle@gmail.com>
2015-02-01 16:12:35 +0100

    Redesign world list page

--- resources/views/world/index.blade.php
+++ resources/views/world/index.blade.php
@@ -7,40 +7,33 @@
                 <h3 class="panel-title">{{trans('worlds.worlds')}}</h3>
             </div>
             <div class="panel-body table-responsive">
-                @if(count($worlds))
-                <table class="table table-hover">
-                    <thead>
-                        <tr>
-                            <th>
-                                {{trans('worlds.world')}}
-                            </th>
-                            <th>
-                                {{trans('members.members')}}
-                            </th>
-                            <th>
-                                {{trans('worlds.creator')}}
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($worlds as $world)
-                        <tr>
-                            <td>
-                                <a href="{{route('worlds.show',[$world->slug()])}}">{{$world->name()}}</a>
-                            </td>
-                            <td>
-                                {{count($world->members())}}
-                            </td>
-                            <td>
-                                {{$world->creator()->name()}}
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-                @else
+                @forelse($worlds as $world)
+                    <div class="well">
+                        <div class="media">
+                            <a class="pull-left hidden-xs" href="{{route('worlds.show', [$world->slug()])}}">
+                                <img class="media-object" src="http://placehold.it/130x130">
+                            </a>
+                            <div class="media-body">
+                                <h4 class="media-heading"><a href="{{route('worlds.show', [$world->slug()])}}">{{$world->name()}}</a></h4>
+                                <p class="text-right">{{$world->brand() . trans('worlds.createdby') . $world->creator()->name()}}</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci est explicabo magnam
+                                    nam optio recusandae, ut voluptate. Animi aperiam at dolorem esse nobis perferendis quibusdam.
+                                    Dignissimos dolorum odio reprehenderit!</p>
+                                <ul class="list-inline list-unstyled">
+                                    <li><span>{{trans('members.members') . ': ' . count($world->members())}} </span></li>
+                                    <li>|</li>
+                                    <li>
+                                        @foreach($world->members() as $member)
+                                            <span>{{$member->name()}}</span>
+                                        @endforeach
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                @empty
                     {{trans('worlds.none')}}
-                @endif
+                @endforelse
             </div>
         </div>
         <div>
