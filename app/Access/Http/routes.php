9e19830a562d74825e032e7da9fbfd84a4b3750f
Lily Belle <tolilybelle@gmail.com>
2015-02-10 07:04:52 +0100

    Move routes from dedicated file to service provider

--- app/Access/Http/routes.php
+++ app/Access/Http/routes.php
@@ -11,64 +11,6 @@
 |
 */
 
-foreach(['lang', ''] as $prefix)
-{
-    $router->group(['prefix' => optional($prefix), 'middleware' => 'localization'], function ($router) use ($prefix) {
-
-        $router->group(['domain' => 'rpgo.' . config('app.tld')], function($router) use ($prefix) {
-
-            $router->get(trans('routes.auth.login'), ['as' => prefix($prefix, 'auth.login'), 'uses' => 'Auth\AuthController@getLogin']);
-
-            $router->get(trans('routes.auth.logout'), ['as' => prefix($prefix, 'auth.logout'), 'uses' => 'Auth\AuthController@getLogout']);
-
-            $router->post(trans('routes.auth.login'), 'Auth\AuthController@postLogin');
-
-            $router->get(trans('routes.auth.register'), ['as' => prefix($prefix, 'auth.register'), 'uses' => 'UserController@create']);
-
-            $router->post(trans('routes.auth.register'), 'UserController@store');
-
-            $router->get(trans('routes.password.email'), ['as' => prefix($prefix, 'password.email'), 'uses' => 'Auth\PasswordController@getEmail']);
-
-            $router->post(trans('routes.password.email'), 'Auth\PasswordController@postEmail');
-
-            $router->get(trans('routes.password.reset') . "/{token}", ['as' => prefix($prefix, 'password.reset'), 'uses' => 'Auth\PasswordController@getReset']);
-
-            $router->post(trans('routes.password.reset'), 'Auth\PasswordController@postReset');
-
-            $router->get(trans("routes.worlds") . "/" . trans("routes.create"),
-                ['as' => prefix($prefix, 'worlds.create'), 'uses' => 'WorldController@create']
-            );
-
-            $router->post(trans("routes.worlds") . "/" . trans("routes.create"),
-                ['as' => prefix($prefix, 'worlds.store'), 'uses' => 'WorldController@store']
-            );
-
-            $router->get(trans("routes.worlds"), ['as' => ($prefix ? $prefix . '.' : '') . 'worlds.index', 'uses' => "WorldController@index"]);
-
-            $router->get('/', ['as' => prefix($prefix, 'home'), 'uses' => 'HomeController@index']);
-
-        });
-
-        $router->group(["domain" => "{slug}.rpgo." . config('app.tld'), 'middleware' => ['worlds', 'member', 'published']], function($router) use ($prefix) {
-
-            $router->get('/', ['as' => prefix($prefix, 'worlds.show'), 'uses' => 'WorldController@show']);
-
-            $router->get(trans('routes.member.create'), ['as' => prefix($prefix, 'member.create'), 'uses' => 'MemberController@create', 'middleware' => 'stranger']);
-
-            $router->post(trans('routes.member.store'), ['as' => prefix($prefix, 'member.store'), 'uses' => 'MemberController@store']);
-
-            $router->get(trans('routes.dashboard.home'), ['as' => prefix($prefix, 'worlds.dashboard.main'), 'uses' => 'DashboardController@main', 'middleware' => 'admin']);
-
-            $router->get(trans('routes.location.index'), ['as' => prefix($prefix, 'location.index'), 'uses' => 'LocationController@index']);
-
-            $router->post(trans('routes.world.publish'), ['as' => prefix($prefix, 'world.publish'), 'uses' => 'WorldController@publish']);
-
-            $router->get('{location}', ['as' => prefix($prefix, 'location.show'), 'uses' => 'LocationController@show'])->where('location', '.+');
-
-        });
-    });
-}
-
 function prefix($prefix, $name)
 {
     return ($prefix ? $prefix . '.' : '') . $name;
