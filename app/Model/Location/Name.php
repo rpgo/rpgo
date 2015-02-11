5e76f38ad0c26b30fd6451ca0a703a9927d6268b
Lily Belle <tolilybelle@gmail.com>
2015-02-11 07:00:11 +0100

    Implement location name

--- app/Model/Location/Name.php
+++ app/Model/Location/Name.php
@@ -1,5 +1,26 @@
 <?php namespace Rpgo\Model\Location;
 
-class Name {
+use Rpgo\Model\Common\Value;
+use Rpgo\Model\Exception\InvalidValueException;
+
+class Name extends Value {
+
+    function __construct($name)
+    {
+        $name = (string) $name;
+
+        $this->checkLength($name);
+
+        parent::__construct($name);
+    }
+
+    private function checkLength($name)
+    {
+        if( strlen(utf8_decode($name)) == 0 )
+            throw new InvalidValueException;
+
+        if( strlen(utf8_decode($name)) > 40 )
+            throw new InvalidValueException;
+    }
 
 }
--- tests/unit/Model/Location/NameSpec.php
+++ tests/unit/Model/Location/NameSpec.php
@@ -7,8 +7,26 @@ use Prophecy\Argument;
 
 class NameSpec extends ObjectBehavior
 {
+    function let()
+    {
+        $this->beConstructedWith('Abydos');
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('Rpgo\Model\Location\Name');
+        $this->shouldHaveType('Rpgo\Model\Common\Value');
+    }
+
+    function it_cannot_be_empty()
+    {
+        $this->shouldThrow('Rpgo\Model\Exception\InvalidValueException')
+            ->during('__construct', ['']);
+    }
+
+    function it_cannot_be_longer_than_40_characters()
+    {
+        $this->shouldThrow('Rpgo\Model\Exception\InvalidValueException')
+            ->during('__construct', ['The Best Possible Stargate Memories To Make In This Life']);
     }
 }
