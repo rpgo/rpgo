cdd9346d4685d787f8a758015271bbe6319accbd
Lily Belle <tolilybelle@gmail.com>
2015-02-12 06:31:05 +0100

    Implement factory of Location

--- app/Model/Location/Factory.php
+++ app/Model/Location/Factory.php
@@ -2,4 +2,72 @@
 
 class Factory {
 
+    private $required = ['name', 'slug'];
+
+    public function make(array $data)
+    {
+        if( ! $this->hasRequiredParameters($data))
+            return false;
+
+        return $this->makeEntity($data);
+    }
+
+    /**
+     * @param array $data
+     * @return Id
+     */
+    private function getId(array $data)
+    {
+        if (array_key_exists('id', $data))
+            return $this->makeId($data['id']);
+        else
+            return $this->makeId();
+    }
+
+    /**
+     * @param null $id
+     * @return Id
+     */
+    private function makeId($id = null)
+    {
+        return new Id($id);
+    }
+
+    private function hasRequiredParameters($data)
+    {
+        foreach($this->required as $key)
+            if( ! array_key_exists($key, $data))
+                return false;
+
+        return true;
+    }
+
+    /**
+     * @param array $data
+     * @return null
+     */
+    private function getContainer(array $data)
+    {
+        if (array_key_exists('container', $data))
+            return $data['container'];
+
+        return null;
+    }
+
+    /**
+     * @param array $data
+     * @return Location
+     */
+    private function makeEntity(array $data)
+    {
+        $id = $this->getId($data);
+
+        $name = new Name($data['name']);
+
+        $slug = new Slug($data['slug']);
+
+        $container = $this->getContainer($data);
+
+        return new Location($id, $name, $slug, $container);
+    }
 }
--- tests/unit/Model/Location/FactorySpec.php
+++ tests/unit/Model/Location/FactorySpec.php
@@ -4,11 +4,82 @@ namespace unit\Rpgo\Model\Location;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Rpgo\Model\Location\Location;
 
 class FactorySpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
         $this->shouldHaveType('Rpgo\Model\Location\Factory');
+    }
+
+    function it_makes_a_new_Location_from_name_and_slug()
+    {
+        $this->make([
+            'name' => 'SGC',
+            'slug' => 'sgc',
+        ])->shouldHaveType('Rpgo\Model\Location\Location');
+    }
+
+    function it_sets_the_name_correctly_on_the_newly_made_Location()
+    {
+        $this->make([
+            'name' => 'SGC',
+            'slug' => 'sgc',
+        ])->name()->shouldBe('SGC');
+    }
+
+    function it_sets_the_slug_correctly_on_the_newly_made_Location()
+    {
+        $this->make([
+            'name' => 'SGC',
+            'slug' => 'sgc',
+        ])->slug()->shouldBe('sgc');
+    }
+
+    function it_doesnt_make_a_new_Location_if_the_name_is_missing()
+    {
+        $this->make(['slug' => 'sgc',])->shouldReturn(false);
+    }
+
+    function it_doesnt_make_a_new_Location_if_the_slug_is_missing()
+    {
+        $this->make(['name' => 'SGC'])->shouldReturn(false);
+    }
+
+    function it_makes_a_new_Location_from_name_slug_and_container_Location(Location $container)
+    {
+        $this->make([
+            'name' => 'SGC',
+            'slug' => 'sgc',
+            'container' => $container,
+        ])->shouldHaveType('Rpgo\Model\Location\Location');
+    }
+
+    function it_sets_the_container_Location_correctly_on_the_newly_made_Location(Location $container)
+    {
+        $this->make([
+            'name' => 'SGC',
+            'slug' => 'sgc',
+            'container' => $container,
+        ])->container()->shouldBe($container);
+    }
+
+    function it_makes_a_new_Location_from_id_name_and_slug()
+    {
+        $this->make([
+            'id' => '5fbb1fe6-f43a-4761-b11d-592d5ca218ca',
+            'name' => 'SGC',
+            'slug' => 'sgc',
+        ])->shouldHaveType('Rpgo\Model\Location\Location');
+    }
+
+    function it_sets_the_id_correctly_on_the_newly_made_Location()
+    {
+        $this->make([
+            'id' => '5fbb1fe6-f43a-4761-b11d-592d5ca218ca',
+            'name' => 'SGC',
+            'slug' => 'sgc',
+        ])->id()->shouldBe('5fbb1fe6-f43a-4761-b11d-592d5ca218ca');
     }
 }
