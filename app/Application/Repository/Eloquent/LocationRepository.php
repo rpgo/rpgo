41ac94ede95ef13422461fac61af3d4d959c1bcd
Lily Belle <tolilybelle@gmail.com>
2015-02-12 06:41:01 +0100

    Fix Location repository

--- app/Application/Repository/Eloquent/LocationRepository.php
+++ app/Application/Repository/Eloquent/LocationRepository.php
@@ -3,16 +3,25 @@
 use Rpgo\Application\Repository\Eloquent\Model\Eloquent as EloquentAdapter;
 use Rpgo\Application\Repository\LocationRepository as LocationRepositoryContract;
 use Rpgo\Application\Repository\RepositoryManager;
+use Rpgo\Model\Location\Factory;
+use Rpgo\Model\Location\Location as Entity;
+use Rpgo\Application\Repository\Eloquent\Model\Location as Eloquent;
 
 class LocationRepository extends Repository implements LocationRepositoryContract {
+    /**
+     * @var Factory
+     */
+    private $factory;
 
     /**
      * @param RepositoryManager $manager
+     * @param Factory $factory
      * @param EloquentAdapter $eloquent
      */
-    public function __construct(RepositoryManager $manager, EloquentAdapter $eloquent)
+    public function __construct(RepositoryManager $manager, Factory $factory, EloquentAdapter $eloquent)
     {
         parent::__construct($manager);
+        $this->factory = $factory;
         $this->eloquent = $eloquent->location();
     }
 
@@ -22,19 +31,34 @@ class LocationRepository extends Repository implements LocationRepositoryContrac
      */
     protected function getEntity($eloquent)
     {
-        return $eloquent;
+        return $this->factory->make($this->getModelData($eloquent));
     }
 
     /**
-     * @param $entity
+     * @param Entity $entity
      * @return array
      */
     protected function getEloquentAttributes($entity)
     {
         return [
-            'container_id' => $entity->container_id,
-            'name' => $entity->name,
-            'slug' => $entity->slug,
+            'id' => $entity->id(),
+            'container_id' => $entity->container()->id(),
+            'name' => $entity->name(),
+            'slug' => $entity->slug(),
+        ];
+    }
+
+    /**
+     * @param Eloquent $eloquent
+     * @return array
+     */
+    private function getModelData($eloquent)
+    {
+        return [
+            'id'   => $eloquent->id,
+            'name' => $eloquent->name,
+            'slug' => $eloquent->slug,
+            'container' => $this->getEntity($eloquent->container)
         ];
     }
 }
