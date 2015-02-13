c3f826e82243b4478db85e9ce2d391fa0ba66991
Lily Belle <tolilybelle@gmail.com>
2015-02-13 06:19:40 +0100

    Prettify location.show view

--- resources/views/location/show.blade.php
+++ resources/views/location/show.blade.php
@@ -1,5 +1,9 @@
 @extends('master.world')
 
 @section('content')
-    {{$location->name()}}
+    <div class="container-fluid">
+        <div class="well well-lg">
+            {{$location->name()}}
+        </div>
+    </div>
 @endsection
