f4b278364de3ec710e9bb6987f8d91765c591d30
Lily Belle <tolilybelle@gmail.com>
2015-02-14 14:54:40 +0100

    Change wording on show location page

--- resources/views/location/show.blade.php
+++ resources/views/location/show.blade.php
@@ -15,14 +15,14 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="btn-group">
-                    <a href="{{route('location.create', [$world->slug(), join('/',$location->path())])}}" class="btn btn-default">Új helyszín</a>
+                    <a href="{{route('location.create', [$world->slug(), join('/',$location->path())])}}" class="btn btn-default">Új alhelyszín</a>
                     <a href="{{route('location.edit', [$world->slug(), join('/',$location->path())])}}" class="btn btn-default">Helyszín szerkesztése</a>
                 </div>
             </div>
             <div class="row">
                 @if(count($location->locations()))
                     <div class="sub-header">
-                        <h3>Helyszínek</h3>
+                        <h3>Alhelyszínek</h3>
                     </div>
                     @foreach($location->locations() as $sublocation)
                         <div class="well"><a href="{{route('location.show', [$world->slug(), join('/', $sublocation->path())])}}">{{$sublocation->name()}}</a></div>
